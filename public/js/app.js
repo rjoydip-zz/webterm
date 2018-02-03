dbd20854c826be08c3770236450e596b030e0bb4
Rjoydip <joydipand@gmail.com>
2018-02-03 17:17:09 +0545

    refactor: code refactor

--- public/js/app.js
+++ public/js/app.js
@@ -1293,39 +1293,51 @@ exports.MouseHelper = MouseHelper;
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_xterm__ = __webpack_require__(13);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_xterm___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_xterm__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_vm__ = __webpack_require__(34);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_vm___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_vm__);
 
+
+
+let line_inputs = [];
 
 const terminalPrefix = "\u001b[1;3;31mxterm\u001b[0m $ ";
 const $terminal = document.getElementById('terminal');
 
+// terminal instance
 const term = new __WEBPACK_IMPORTED_MODULE_0_xterm__["Terminal"]({
     cursorBlink: true,
     rows: 15
 });
 
-let line_inputs = [];
-
+// re-useable terminal prefixer as terminal prompt
 term.prompt = () => {
     term.write('\r\n' + terminalPrefix);
 };
 
+// opening terminal and initilize default text
 term.open($terminal);
 term.writeln('Welcome to Web Terminal');
 term.prompt();
 
+// let res = vm.runInNewContext('a + 5', { a : 100 });
+// vm.runInNewContext('console.log("Hello")');
+
+// terminal key input event
 term.on('key', (key, ev) => {
-    let printable = (!ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey), 
+    let printable = (!ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey),
         kc = ev.keyCode;
 
-    console.log(kc, key)
+    // console.log(kc, key)
 
     if (kc == 13) {
-        return line_inputs = [] , term.prompt();
+        // Enter: re initilize line input array and show terminal prefixer
+        return line_inputs = [], term.prompt();
     } else if (kc == 8) {
-        // remove previous character when backspace pressed
+        // Backspace: remove previous character when key pressed
         return line_inputs.length !== 0 ? term.write('\b \b') : false, line_inputs.pop();
     } else if (printable) {
-        return line_inputs.push(key) , term.write(key);
+        // print the key and putting in the line inputes array
+        return line_inputs.push(key), term.write(key);
     }
 });
 
@@ -6494,6 +6506,165 @@ exports.BellSound = 'data:audio/wav;base64,UklGRigBAABXQVZFZm10IBAAAAABAAEARKwAA
 
 //# sourceMappingURL=Sounds.js.map
 
+
+/***/ }),
+/* 34 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var indexOf = __webpack_require__(35);
+
+var Object_keys = function (obj) {
+    if (Object.keys) return Object.keys(obj)
+    else {
+        var res = [];
+        for (var key in obj) res.push(key)
+        return res;
+    }
+};
+
+var forEach = function (xs, fn) {
+    if (xs.forEach) return xs.forEach(fn)
+    else for (var i = 0; i < xs.length; i++) {
+        fn(xs[i], i, xs);
+    }
+};
+
+var defineProp = (function() {
+    try {
+        Object.defineProperty({}, '_', {});
+        return function(obj, name, value) {
+            Object.defineProperty(obj, name, {
+                writable: true,
+                enumerable: false,
+                configurable: true,
+                value: value
+            })
+        };
+    } catch(e) {
+        return function(obj, name, value) {
+            obj[name] = value;
+        };
+    }
+}());
+
+var globals = ['Array', 'Boolean', 'Date', 'Error', 'EvalError', 'Function',
+'Infinity', 'JSON', 'Math', 'NaN', 'Number', 'Object', 'RangeError',
+'ReferenceError', 'RegExp', 'String', 'SyntaxError', 'TypeError', 'URIError',
+'decodeURI', 'decodeURIComponent', 'encodeURI', 'encodeURIComponent', 'escape',
+'eval', 'isFinite', 'isNaN', 'parseFloat', 'parseInt', 'undefined', 'unescape'];
+
+function Context() {}
+Context.prototype = {};
+
+var Script = exports.Script = function NodeScript (code) {
+    if (!(this instanceof Script)) return new Script(code);
+    this.code = code;
+};
+
+Script.prototype.runInContext = function (context) {
+    if (!(context instanceof Context)) {
+        throw new TypeError("needs a 'context' argument.");
+    }
+    
+    var iframe = document.createElement('iframe');
+    if (!iframe.style) iframe.style = {};
+    iframe.style.display = 'none';
+    
+    document.body.appendChild(iframe);
+    
+    var win = iframe.contentWindow;
+    var wEval = win.eval, wExecScript = win.execScript;
+
+    if (!wEval && wExecScript) {
+        // win.eval() magically appears when this is called in IE:
+        wExecScript.call(win, 'null');
+        wEval = win.eval;
+    }
+    
+    forEach(Object_keys(context), function (key) {
+        win[key] = context[key];
+    });
+    forEach(globals, function (key) {
+        if (context[key]) {
+            win[key] = context[key];
+        }
+    });
+    
+    var winKeys = Object_keys(win);
+
+    var res = wEval.call(win, this.code);
+    
+    forEach(Object_keys(win), function (key) {
+        // Avoid copying circular objects like `top` and `window` by only
+        // updating existing context properties or new properties in the `win`
+        // that was only introduced after the eval.
+        if (key in context || indexOf(winKeys, key) === -1) {
+            context[key] = win[key];
+        }
+    });
+
+    forEach(globals, function (key) {
+        if (!(key in context)) {
+            defineProp(context, key, win[key]);
+        }
+    });
+    
+    document.body.removeChild(iframe);
+    
+    return res;
+};
+
+Script.prototype.runInThisContext = function () {
+    return eval(this.code); // maybe...
+};
+
+Script.prototype.runInNewContext = function (context) {
+    var ctx = Script.createContext(context);
+    var res = this.runInContext(ctx);
+
+    forEach(Object_keys(ctx), function (key) {
+        context[key] = ctx[key];
+    });
+
+    return res;
+};
+
+forEach(Object_keys(Script.prototype), function (name) {
+    exports[name] = Script[name] = function (code) {
+        var s = Script(code);
+        return s[name].apply(s, [].slice.call(arguments, 1));
+    };
+});
+
+exports.createScript = function (code) {
+    return exports.Script(code);
+};
+
+exports.createContext = Script.createContext = function (context) {
+    var copy = new Context();
+    if(typeof context === 'object') {
+        forEach(Object_keys(context), function (key) {
+            copy[key] = context[key];
+        });
+    }
+    return copy;
+};
+
+
+/***/ }),
+/* 35 */
+/***/ (function(module, exports) {
+
+
+var indexOf = [].indexOf;
+
+module.exports = function(arr, obj){
+  if (indexOf) return arr.indexOf(obj);
+  for (var i = 0; i < arr.length; ++i) {
+    if (arr[i] === obj) return i;
+  }
+  return -1;
+};
 
 /***/ })
 /******/ ]);
--- src/app.js
+++ src/app.js
@@ -1,37 +1,47 @@
 import {
     Terminal
 } from 'xterm';
+import * as vm from 'vm';
+
+let line_inputs = [];
 
 const terminalPrefix = "\u001b[1;3;31mxterm\u001b[0m $ ";
 const $terminal = document.getElementById('terminal');
 
+// terminal instance
 const term = new Terminal({
     cursorBlink: true,
     rows: 15
 });
 
-let line_inputs = [];
-
+// re-useable terminal prefixer as terminal prompt
 term.prompt = () => {
     term.write('\r\n' + terminalPrefix);
 };
 
+// opening terminal and initilize default text
 term.open($terminal);
 term.writeln('Welcome to Web Terminal');
 term.prompt();
 
+// let res = vm.runInNewContext('a + 5', { a : 100 });
+// vm.runInNewContext('console.log("Hello")');
+
+// terminal key input event
 term.on('key', (key, ev) => {
-    let printable = (!ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey), 
+    let printable = (!ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey),
         kc = ev.keyCode;
 
-    console.log(kc, key)
+    // console.log(kc, key)
 
     if (kc == 13) {
-        return line_inputs = [] , term.prompt();
+        // Enter: re initilize line input array and show terminal prefixer
+        return line_inputs = [], term.prompt();
     } else if (kc == 8) {
-        // remove previous character when backspace pressed
+        // Backspace: remove previous character when key pressed
         return line_inputs.length !== 0 ? term.write('\b \b') : false, line_inputs.pop();
     } else if (printable) {
-        return line_inputs.push(key) , term.write(key);
+        // print the key and putting in the line inputes array
+        return line_inputs.push(key), term.write(key);
     }
 });
