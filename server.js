f5da59af96f0aa5eea19e68a62c71335baa8cb9c
Rjoydip <joydipand@gmail.com>
2018-02-04 21:30:53 +0545

    fix: heroku setup 4

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var path = require('path'),
     express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    io = require('socket.io')(app);
+    io = require('socket.io')(server);
 
 app.use(express.static('public'));
 app.get('/', function (req, res) {
